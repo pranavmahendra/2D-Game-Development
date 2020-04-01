3036d16768fa2333fe3ddc157a6ddf0ffcdb9ae8
Pranav Mahendra <>
2020-04-01 19:11:35 +0530

    Switch Audio

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio; 
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 
 public class SoundManager : MonoBehaviour
@@ -11,6 +12,8 @@ public class SoundManager : MonoBehaviour
     private static SoundManager _instance;
 
     public static SoundManager Instance { get { return _instance; } }
+
+    private Scene activeScene;
 
     public  AudioSource audio;
 
@@ -27,18 +30,39 @@ public class SoundManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        
+        
     }
 
     private void Start()
     {
-        audio = GetComponent<AudioSource>();
-        audio.clip = clips[0];
-        audio.Play();
-        
-        
+        activeScene = SceneManager.GetActiveScene();
+        switchAudio();
+      
     }
 
-    
+    public void switchAudio()
+    {
+        // If level1 with build index 1 is playing Clip1 from array should play.
+        if (activeScene.buildIndex == 1)
+        {
+            Debug.Log("Level music playing");
+            audio = GetComponent<AudioSource>();
+            audio.clip = clips[1];
+            audio.Play();
+        }
+
+        else
+        {
+            Debug.Log("Main Menu music playing");
+            audio = GetComponent<AudioSource>();
+            audio.clip = clips[0];
+            audio.Play();
+        }
+    }
 
 }
+
+   
 
